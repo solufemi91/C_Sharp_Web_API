ec2690da9caacc00e911e70d9c061b3385249b15
Steve Olufemi <solufemi-olukanni@hotmail.com>
2018-11-02 17:33:46 +0000

    final commit

--- XUnitTestProject1/DestinationsControllerTests.cs
+++ XUnitTestProject1/DestinationsControllerTests.cs
@@ -4,7 +4,6 @@ using WebApi.Controllers;
 using System.Threading.Tasks;
 using System.Linq;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
 using Moq;
 using System.Collections.Generic;
@@ -15,7 +14,7 @@ namespace WebApiTestProject
     {
         [Fact]
 
-        public void TestGetMethodReturnsCollectionOfDestinationObjects()
+        public async Task TestGetMethodReturnsCollectionOfDestinationObjectsAsync()
         {
             var destinations = new List<Destination>
             {
@@ -48,11 +47,83 @@ namespace WebApiTestProject
                }
             };
 
-            var mock = new Mock<IDestinationsController>();
-            mock.Setup(x => x.Get()).ReturnsAsync(destinations.AsEnumerable());
+            var expectedResult = new List<Destination>
+            {
+               new Destination
+               {
+                   DestinationId = 1,
+                   Country = "England",
+                   City = "London"
+               },
+
+               new Destination
+               {
+                   DestinationId = 2 ,
+                   Country = "England",
+                   City = "Manchester"
+               },
+
+               new Destination
+               {
+                   DestinationId = 3 ,
+                   Country = "Spain",
+                   City = "Madrid"
+               },
+
+               new Destination
+               {
+                   DestinationId = 4 ,
+                   Country = "France",
+                   City = "Paris"
+               }
+            };
+
+            var mock = new Mock<IDataProvider>();
+            mock.Setup(x => x.GetDestinations()).ReturnsAsync(destinations.AsEnumerable());
+
+            var destinationsController = new DestinationsController(mock.Object);
+
+            var collectionOfDestinations = await destinationsController.Get();
+                
+            collectionOfDestinations.Should().BeEquivalentTo(expectedResult.AsEnumerable(), "this is the list of destinations from the database");
+
         }
 
-      
+
+        [Fact]
+
+        public async Task TestParameterisedGetMethodReturnsADestinationObjectsAsync()
+        {
+
+            var destination = new Destination
+            {
+                DestinationId = 1,
+                Country = "England",
+                City = "London"
+            };
+
+            var expectedResult = new Destination
+            {
+                DestinationId = 1,
+                Country = "England",
+                City = "London"
+            };
+
+
+
+
+            var mock = new Mock<IDataProvider>();
+            mock.Setup(x => x.GetDestination(1)).ReturnsAsync(destination);
+
+            var destinationsController = new DestinationsController(mock.Object);
+
+            var destinationObject = await destinationsController.Get(1);
+
+            destinationObject.Should().BeEquivalentTo(expectedResult, "this is the list of destinations from the database");
+
+        }
+
+
 
     }
 }
